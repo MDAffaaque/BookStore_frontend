1c8a8a454e8a6ec6340cb6cf8b5278938f2b7f73
MDAffaaque <mdsahil150.ms@gmail.com>
2024-03-04 00:47:11 +0530

    uploaded successfully

---
+++ frontend/src/Pages/ShowBook.jsx
@@ -0,0 +1,61 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import BackButton from '../Components/BackButton'
+import Spinner from '../Components/Spinner'
+
+const ShowBook = () => {
+  const [Book,setBooks]=useState({});
+  const [loading,setLoading]=useState(false);
+  const {id}=useParams();
+  useEffect(()=>{
+    setLoading(true);
+    axios.get(`https://book-store-jade-delta.vercel.app/books/${id}`)
+    .then((res)=>{
+      setBooks(res.data);
+      setLoading(false);
+    })
+    .catch((error)=>{
+      console.log(error);
+      setLoading(false);
+    })
+  },[])
+  return (
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className='text-3xl my-4'>Show Book</h1>
+      {loading?(<Spinner/>):(
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'>Id</span>
+          <span>{Book._id}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'>Title</span>
+          <span>{Book.title}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'>Author</span>
+          <span>{Book.author}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
+          <span>{Book.publishYear}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'>Create Time</span>
+          <span>{new Date(Book.createdAt).toString()}</span>
+        </div>
+        <div className='my-4'>
+          <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
+          <span>{new Date(Book.updatedAt).toString()}</span>
+        </div>
+
+      </div>
+      
+      )}
+    </div>
+  )
+}
+
+export default ShowBook
